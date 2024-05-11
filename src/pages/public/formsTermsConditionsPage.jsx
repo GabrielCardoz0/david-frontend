752eb205654f87c3fe8cd7f25623d27ec1d8397f
GabrielCardoz0 <gabriel.scardozo7@gmail.com>
2024-05-11 14:32:01 -0300

    refact: termos e condições de uso

--- src/pages/public/formsTermsConditionsPage.jsx
+++ src/pages/public/formsTermsConditionsPage.jsx
@@ -35,7 +35,25 @@ export default function FormsTermsConditionsPage() {
             <SquareForm>
                 <h1 style={{ fontSize:"14px" }}>Termos e condições de uso</h1>
                 <br/>
-                <p style={{ fontSize:"12px" }}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                
+                <p style={{ fontSize: "12px", textAlign: "justify" }}>
+                    Este formulário é um serviço oferecido pela empresa David & Companhia.
+                    Ao preencher este formulário, você concorda com os termos e condições
+                    de uso da empresa, concordando em disponibilizar seus dados
+                    pessoais para fins de marketing e comunicação, de acordo com a legislação vigente.
+                </p>
+
+                <br/>
+
+                <ul style={{ fontSize: "12px", textAlign: "justify" }}>
+                    <li>Ao utilizar esta ferramenta, você concorda em disponibilizar seus dados pessoais para a empresa David & Companhia.</li>
+                    <br/>
+                    <li>A empresa poderá utilizar esses dados para fins de marketing e comunicação, de acordo com a legislação vigente.</li>
+                    <br/>
+                    <li>A empresa se compromete a proteger e manter a confidencialidade dos seus dados pessoais.</li>
+                    <br/>
+                    <li>Você pode solicitar a exclusão dos seus dados a qualquer momento, entrando em contato com a empresa.</li>
+                </ul>
 
                 <br/>
                 
